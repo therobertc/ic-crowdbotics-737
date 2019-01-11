c3ccea5136e059c711f4df08b89fa3336dc992c8
nikolal <iamnikolalukic@gmail.com>
2019-01-11 13:25:53 +0100

    Update colors

--- src/theme/colors.js
+++ src/theme/colors.js
@@ -1,29 +1,18 @@
 const colors = {
-  white: '#ffffff',
-  black: '#000000',
-
-  grey: '#808080',
-  darkGrey: '#A9A9A9',
-  lightGrey: '#D3D3D3',
-
-  red: '#ff0000',
-  darkRed: '#8B0000',
-  lightRed: '#ff6666',
-
-  blue: '#0000FF',
-  darkBlue: '#00008B',
-  lightBlue: '80dfff',
-
-  green: '#008000',
-  darkGreen: '#006400',
-  lightGreen: '#99e699',
-
-  brown: '#663300',
-  orange: '#ff6600',
-  pink: '#ff66ff',
-  violet: '#6600cc',
-  yellow: '#FFFF00'
-
+  // Core
+  background: '#ffffff', // background
+  primary: '#52CBC2', //  darker
+  secondary: '#90DBD7', // lighter
+  oposite: '#FF5F6A', //
+  error: '#ff0000',
+  // Text
+  textLight: '#3E4A59',
+  textMedium: '#425256',
+  textDark: '#4C495B',
+  // Grey shades
+  greyLight: '#F7F7F7',
+  greyMedium: '#ECF1F8',
+  greyDark: '#3E4A59',
 };
 
 export default colors;
