f131efdc6fd89b1c65fbf04143e3bb24e1b91a3e
goldstar0415 <topdeveloper1212@gmail.com>
2019-01-28 19:34:17 +0200

    [new] complete the signup Bot at Dialogflow side [new] Integration signupbot with App [new] Integration app with firebase [new] Signup, sendEmailVerification function.

--- config/firebase.js
+++ config/firebase.js
@@ -1,3 +1,6 @@
+import * as firebase from 'firebase';
+import 'firebase/firestore';
+
 export const firebaseConfig = {
   apiKey: 'AIzaSyDTEm1-9dt5hBFBhg7-_2I-ZFNrqiVWxqU',
   authDomain: 'emmaapi.firebaseapp.com',
@@ -6,3 +9,22 @@ export const firebaseConfig = {
   storageBucket: 'emmaapi.appspot.com',
   messagingSenderId: '1075847453292'
 };
+
+_saveUserInfo = (uid, userInfo) => {
+  var database = firebase.firestore().collection('user_info')
+  var obj = {
+      'name': userInfo.name
+  }
+  database.doc(uid).set(obj);
+}
+
+export const _signupAPI = (userInfo) => {
+  firebase.auth().createUserWithEmailAndPassword(userInfo.email, userInfo.password)
+  .then((res) => {
+      var user = firebase.auth().currentUser;
+      this._saveUserInfo(user.uid, userInfo);
+      user.sendEmailVerification();
+      return res.json();
+  })
+  .catch((err) => err.message)
+}
--- src/components/signup/Chatbot.js
+++ src/components/signup/Chatbot.js
@@ -3,25 +3,107 @@ import {
   StyleSheet, 
   Text, 
   View,
-  TextInput,
+  Alert,
   TouchableOpacity
 } from 'react-native';
 import { Icon } from 'expo';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
-import { GiftedChat, Bubble } from 'react-native-gifted-chat';
+import { GiftedChat, Time, Bubble, InputToolbar, Send } from 'react-native-gifted-chat';
 
 import { saveEmailAction, savePhoneNumberAction  } from '../../reducer/signup.js';
+import { fetchResponseFromDialogflow } from '../../../config/api.js';
+import { _validateEmail } from '../../../config/util.js';
+import { _signupAPI } from '../../../config/firebase.js';
 import { metrics, colors, fonts } from '../../theme/index.js';
 
 
 
 class SignupChatbot extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      is_buttons: false,
+      type: 'input',
+      intent_name: ''
+    }
 
-  state = {
-    user_msg: '',
-    termsAccepted: false,
-    textinput_height: 25
+    this._renderBubble = this._renderBubble.bind(this);
+    this._renderInputToolbar = this._renderInputToolbar.bind(this);
+    this._send = this._send.bind(this);
+  } 
+
+  componentDidMount() {
+    this._fetchResponse('hi')
+  }
+
+  _processResponse = (res) => {
+    console.log('Res', res)
+    const payload = res.result.fulfillment.messages[0].payload;
+    const intent_name = res.result.metadata.intentName;
+
+    if (intent_name.toLowerCase().includes('password')) {
+      const user_info = {
+        name: this.state.name.trim() || '',
+        email: this.state.email.trim().toLowerCase() || '',
+        password: this.state.password
+      }
+
+      _signupAPI(user_info).then((res) => {
+        console.log(res)
+      }).catch((err) => {
+        console.log(err)
+      })
+    }
+
+    const message = {
+      _id: Math.random() * 1000000,
+      text: payload.messages.join('\n\n'),
+      createdAt: new Date(),
+      user: {
+        _id: 2,
+        name: 'bot'
+      },
+      data: payload.data
+    }
+    this.setState((previousState) => {
+      return {
+        messages: GiftedChat.append(previousState.messages, message),
+        intent_name: intent_name
+      };
+    });
+  }
+
+  _storeInfo = (msg) => {
+    const { intent_name } = this.state;
+    const intentName = intent_name.toLowerCase();
+    if (intentName.includes('signup')) {
+      this.setState({
+        name: msg
+      })
+    } else if (intentName.includes('name')) {
+      this.setState({
+        email: msg
+      })
+    } else if (intentName.includes('email')) {
+      this.setState({
+        password: msg
+      })
+      msg = 'password ' + msg;
+    }
+    
+    return msg
+  }
+
+  _fetchResponse = (msg) => {
+    msg = this._storeInfo(msg)
+
+    fetchResponseFromDialogflow(msg)
+    .then((res) => {
+      this._processResponse(res)
+    }).catch((err) => {
+      console.log(err)
+    })
   }
 
   _handleText = (event) => {
@@ -30,6 +112,100 @@ class SignupChatbot extends Component {
       user_msg: text,
       textinput_height: contentSize.height > 100 ? 100 : contentSize.height
     })
+  }
+
+  _send = (msg = []) => {
+    if (this.state.intent_name.toLowerCase().includes('name') && _validateEmail(msg[0].text) == false) {
+      Alert.alert(
+        'Notice',
+        'Please enter the valid email address',
+        [
+            {text: 'OK', onPress: () => console.log('Got it.')}
+        ],
+        {cancelable: true}
+      );
+      return
+    }
+
+    if (msg[0].text.trim() == '' || msg[0].text == undefined) {
+      Alert.alert(
+        'Notice',
+        'You can\'t put the empty string as answer',
+        [
+            {text: 'OK', onPress: () => console.log('Got it.')}
+        ],
+        {cancelable: true}
+      )
+      return
+    }
+
+    this.setState((previousState) => {
+      return {
+        messages: GiftedChat.append(previousState.messages, msg)
+      };
+    });
+
+    this._fetchResponse(msg[0].text)
+  }
+
+  send = (msg) => {
+    const message = {
+      _id: Math.random() * 1000000,
+      text: msg,
+      createdAt: new Date(),
+      user: {
+        _id: 1
+      }
+    }
+    var messages = [];
+    messages.push(message);
+    this._send(messages);
+  }
+
+  _renderBubble = (props) => {
+    const { currentMessage } = props;
+    if (currentMessage.user._id != 2)
+      return <Bubble {...props}/>
+    return (
+      <View>
+        <View style={styles.messageWrapper}>
+          <Text>{currentMessage.text}</Text>
+          <Time {...props} containerStyle={{left: {marginLeft: 0, marginTop: 5, marginBottom: 0}}}/>
+        </View>
+
+        {
+          currentMessage.data.type == 'button' && <View style={{alignItems: 'center', marginBottom: 30, marginTop: 10}}>
+            {
+              currentMessage.data.buttons.map((buttonText, index) => {
+                return (
+                  <TouchableOpacity key={index} style={styles.buttonContainer} onPress={() => this.send(buttonText)}>
+                    <Text>{buttonText}</Text>
+                  </TouchableOpacity>
+                )
+              })
+            }
+          </View>
+        }
+      </View>
+    )
+  }
+
+  _renderInputToolbar = (props) => {
+    return (
+      <InputToolbar {...props} containerStyle={{borderTopWidth: 0}} primaryStyle={styles.footContainer}/>
+    )
+  }
+
+  _renderSend = (props) => {
+    return (
+      <Send {...props} dis containerStyle={styles.sendButtonContainer}>
+        <Icon.Ionicons
+          name={'md-send'}
+          color={colors.send}
+          size={26}
+        />
+      </Send>
+    )
   }
 
   render(){
@@ -41,28 +217,18 @@ class SignupChatbot extends Component {
             <Text style={styles.headerText}>Hi I am Emma</Text>
         </View>
 
-        {/* <GiftedChat
+        <GiftedChat
           messages={this.state.messages}
+          user={{
+            _id: 1
+          }}
+          alwaysShowSend={true}
           onSend={this._send}
-          renderBubble={this.renderBubble}
-        /> */}
-
-        <View style={styles.footContainer}>
-          <TextInput
-            multiline
-            placeholder={'Write something here'}
-            value={this.state.user_msg}
-            onContentSizeChange={this._handleText.bind(this)}
-            style={[styles.chatInput, {height: this.state.textinput_height}]}
-          />
-          <TouchableOpacity>
-            <Icon.Ionicons
-              name={'md-send'}
-              color={colors.send}
-              size={26}
-            />
-          </TouchableOpacity>
-        </View>
+          renderAvatar={null}
+          renderBubble={this._renderBubble}
+          renderSend={this._renderSend}
+          renderInputToolbar={this._renderInputToolbar}
+        />
       </View>
     );
   }
@@ -96,15 +262,34 @@ const styles = StyleSheet.create({
     color: colors.secondary
   },
   footContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
     borderColor: 'rgba(217, 226, 233, 0.5)',
     borderRadius: 6,
     borderWidth: 0.5,
-    padding: 10,
+    alignItems: 'center'
   },
   chatInput: {
     flex: 1,
     marginRight: 10,
+  },
+  messageWrapper: {
+    padding: 10,
+    borderRadius: 6,
+    backgroundColor: '#F7F7F7',
+    alignItems: 'flex-start'
+  },
+  buttonContainer: {
+    height: 45,
+    marginTop: 10,
+    paddingRight: 20,
+    paddingLeft: 20,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 6,
+    borderWidth: 0.5,
+    borderColor: 'rgba(217, 226, 233, 0.5)',
+  },
+  sendButtonContainer: {
+    justifyContent: 'center',
+    marginRight: 10
   }
 });
